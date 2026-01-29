a34070532ea1b0c632d9002dd7e6158a1fd3ec10
Γιάννης Λινάρδος <johnlinard@192.168.1.42>
2026-01-29 17:09:48 +0200

    first files

---
+++ src/bank.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib>
+#include "messages.h"
+
+typedef struct Account{
+    char name[50];
+    char password[30];
+}
+
+
+
+
+int main(){
+
+
+
+}
---
+++ src/hi_mes.c
@@ -0,0 +1,7 @@
+void intro(){
+
+
+    printf("Hello")
+
+
+}
